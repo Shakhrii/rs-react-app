d35603a443392ec3fb7de3b65b1e4d939b0c7c86
Shakhrii <sh.ramaldanova@gmail.com>
2025-07-14 01:15:30 +0300

    fix: fix prettier config

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,15 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import react from "eslint-plugin-react";
-import tseslint from "typescript-eslint";
-import eslintConfigPrettier from "eslint-config-prettier";
-import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
-import reactCompiler from "eslint-plugin-react-compiler";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import react from 'eslint-plugin-react';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import reactCompiler from 'eslint-plugin-react-compiler';
 
 export default tseslint.config(
-  { ignores: ["dist"] },
+  { ignores: ['dist'] },
   {
     extends: [
       js.configs.recommended,
@@ -17,40 +17,40 @@ export default tseslint.config(
       eslintConfigPrettier,
       eslintPluginPrettier,
     ],
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
       react,
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-      "react-compiler": reactCompiler,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'react-compiler': reactCompiler,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
+      'react-refresh/only-export-components': [
+        'warn',
         { allowConstantExport: true },
       ],
-      "react-compiler/react-compiler": "error",
+      'react-compiler/react-compiler': 'error',
       ...react.configs.recommended.rules,
-      ...react.configs["jsx-runtime"].rules,
-      quotes: ["error", "single", { avoidEscape: true }],
-      "prettier/prettier": [
-        "error",
+      ...react.configs['jsx-runtime'].rules,
+      quotes: ['error', 'single', { avoidEscape: true }],
+      'prettier/prettier': [
+        'error',
         {
           singleQuote: true,
-          trailingComma: "es5",
+          trailingComma: 'es5',
           semi: true,
         },
       ],
     },
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
-  },
+  }
 );
