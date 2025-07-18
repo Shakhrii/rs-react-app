c7806bee5eea52ec6b09dd614e0d0c39378da686
Shakhrii <sh.ramaldanova@gmail.com>
2025-07-19 00:58:43 +0300

    feat: add test for checking save search term to LS

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,11 +1,19 @@
-import { describe, it, expect } from 'vitest';
+import { describe, expect, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import App from './App';
 
-describe('something truthy and falsy', () => {
-  it('true to be true', () => {
-    expect(true).toBe(true);
-  });
+describe('User interaction test', () => {
+  it('saves search term to localStorage when search button is clicked', async () => {
+    render(<App />);
+    const input = screen.getByRole('textbox');
+    const button = screen.getByText('Search');
+    const SEARCH_TERM_KEY = 'search_term';
 
-  it('false to be false', () => {
-    expect(false).toBe(false);
+    await userEvent.clear(input);
+    await userEvent.type(input, 'test');
+    await userEvent.click(button);
+    expect(localStorage.getItem(SEARCH_TERM_KEY)).toBe('test');
   });
 });
