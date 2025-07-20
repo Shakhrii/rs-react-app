f4072db40e76c6cc94e2b31f546dfb8b5a98452d
Shakhrii <sh.ramaldanova@gmail.com>
2025-07-20 20:21:41 +0300

    feat: add test for checking display fallback UI ErrorBoundary

--- src/components/error/ButtonBoundaryErrorView.test.tsx
+++ src/components/error/ButtonBoundaryErrorView.test.tsx
@@ -4,21 +4,21 @@ import '@testing-library/jest-dom';
 import { ButtonBoundaryErrorView } from './ButtonBoundaryErrorView';
 
 describe('Error Button Tests', async () => {
-    const consoleErrorMock = vi.spyOn(console, 'error');
+  const consoleErrorMock = vi.spyOn(console, 'error');
 
-    beforeEach(() => {
-        consoleErrorMock.mockImplementation(() => {});
-    });
+  beforeEach(() => {
+    consoleErrorMock.mockImplementation(() => {});
+  });
 
-    afterEach(() => {
-        consoleErrorMock.mockRestore();
-    })
+  afterEach(() => {
+    consoleErrorMock.mockRestore();
+  });
 
-    it('throws error when test button is clicked', async () => {
-        expect(() => {
-          const { getByRole } = render(<ButtonBoundaryErrorView />);
-          const button = getByRole('button');
-          fireEvent.click(button);
-        }).toThrow('Render Error from ButtonBoundaryErrorView');
-    });  
-})
+  it('throws error when test button is clicked', async () => {
+    expect(() => {
+      const { getByRole } = render(<ButtonBoundaryErrorView />);
+      const button = getByRole('button');
+      fireEvent.click(button);
+    }).toThrow('Render Error from ButtonBoundaryErrorView');
+  });
+});
