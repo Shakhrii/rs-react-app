a4e1960cbf3eff13bd99540ae81599b361420e0e
Shakhrii <sh.ramaldanova@gmail.com>
2025-07-20 13:01:59 +0300

    fix: change logic of App and CardListView components

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,36 +1,78 @@
-import { afterEach, describe, expect, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import App from './App';
-
-afterEach(() => {
-  localStorage.clear();
-});
+import type { Pokemon } from '../types/types';
+import * as api from '../api/Api'; 
 
 describe('User interaction test', () => {
-  it('saves search term to localStorage when search button is clicked', async () => {
-    render(<App />);
-    const input = screen.getByRole('textbox');
-    const button = screen.getByText('Search');
-    const SEARCH_TERM_KEY = 'search_term';
-
-    await userEvent.clear(input);
-    await userEvent.type(input, 'test');
-    await userEvent.click(button);
-    expect(localStorage.getItem(SEARCH_TERM_KEY)).toBe('test');
-  });
-});
+    it('saves search term to localStorage when search button is clicked', async () => {
+      render(<App />);
+      const input = screen.getByRole('textbox');
+      const button = screen.getByText('Search');
+      const SEARCH_TERM_KEY = 'search_term';
+  
+      await userEvent.clear(input);
+      await userEvent.type(input, 'test');
+      await userEvent.click(button);
+      expect(localStorage.getItem(SEARCH_TERM_KEY)).toBe('test');
+    });
+}); 
 
 describe('Local Storage integration', () => {
-  it('retrieves saved search term on component mount', async () => {
-    const searchTerm = 'value';
-    const SEARCH_TERM_KEY = 'search_term';
-    localStorage.setItem(SEARCH_TERM_KEY, searchTerm);
-
-    render(<App />);
-    const input = screen.getByRole('textbox');
-    expect(localStorage.getItem(SEARCH_TERM_KEY)).toBe(searchTerm);
-    expect(input).toHaveValue(searchTerm);
-  });
+    it('retrieves saved search term on component mount', async () => {
+      const searchTerm = 'value';
+      const SEARCH_TERM_KEY = 'search_term';
+      localStorage.setItem(SEARCH_TERM_KEY, searchTerm);
+  
+      render(<App />);
+      const input = screen.getByRole('textbox');
+      expect(localStorage.getItem(SEARCH_TERM_KEY)).toBe(searchTerm);
+      expect(input).toHaveValue(searchTerm);
+    });
 });
+
+describe('Rendering tests', () => {
+    beforeEach(() => {
+        const testPokemons: Pokemon[] = [];
+  
+        for (let i = 0; i < 10; i++) {
+          const pokemon = {
+            id: i,
+            name: `name${i}`,
+            avatar: `avatar${i}`,
+            weight: i,
+            height: i,
+            abilities: `abilities${i}`,
+          };
+          testPokemons.push(pokemon);
+        }
+  
+        vi.spyOn(api, "getPokemons").mockImplementation(() => 
+          new Promise((resolve) => 
+            setTimeout(() => resolve(testPokemons), 500)
+          )
+        );
+    });
+      
+    afterEach(() => {
+        vi.clearAllMocks();
+    });
+
+    it('show loading state while fetching data', async () => {
+      render(<App />);
+      
+      const button = screen.getByText('Search');
+        await userEvent.click(button);
+
+      expect(screen.getByRole('status')).toBeInTheDocument();
+      
+      await waitFor(() => {
+        expect(screen.queryByRole('status')).toBeNull();
+      }, {timeout: 3000});
+    });
+  });
+  
+  
+  
