17aa61dd4af34ad52310633fa7125522c1740d88
Shakhrii <sh.ramaldanova@gmail.com>
2025-07-25 23:06:13 +0300

    feat: add selected state on menu

--- src/components/header/HeaderView.tsx
+++ src/components/header/HeaderView.tsx
@@ -1,17 +1,21 @@
 import { Link } from 'react-router';
 import type { HeaderViewProps } from '../../types/types';
+import { useState } from 'react';
 
 export default function HeaderView({ children }: HeaderViewProps) {
+  const [isActivePokemons, setActivePokemons] = useState(true);
+
   return (
     <header className="pb-6">
       <h1 className="text-neutral-500 text-4xl pb-6">Pokemons Api</h1>
       <nav>
-        <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+        <div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200">
           <ul className="flex flex-wrap -mb-px items-center justify-center">
             <li className="me-2">
               <Link
                 to="/"
-                className="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500"
+                onClick={() => setActivePokemons(true)}
+                className={`inline-block p-4 border-b-2  rounded-t-lg ${isActivePokemons ? 'border-blue-600 text-blue-600' : 'border-transparent hover:text-gray-600 hover:border-gray-300'}`}
                 aria-current="page"
               >
                 Pokemons
@@ -20,7 +24,8 @@ export default function HeaderView({ children }: HeaderViewProps) {
             <li className="me-2">
               <Link
                 to="/about"
-                className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
+                onClick={() => setActivePokemons(false)}
+                className={`inline-block p-4 border-b-2  rounded-t-lg ${!isActivePokemons ? 'border-blue-600 text-blue-600' : 'border-transparent hover:text-gray-600 hover:border-gray-300'}`}
               >
                 About
               </Link>
