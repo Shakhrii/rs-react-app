2ecd27405cfc7242548d019b5fbc385f0a06fea1
Shakhrii <sh.ramaldanova@gmail.com>
2025-08-24 20:13:09 +0300

    feat: add animation to last item

--- src/components/data-list/DataList.tsx
+++ src/components/data-list/DataList.tsx
@@ -1,11 +1,21 @@
 import { selectFormDataItems } from '../../store/slices/formDataItems.slice';
 import { useAppSelector } from '../../store/store';
 import DataItem from './data-item/DataItem';
+import styles from './DataList.module.css';
 
 function DataList() {
   const dataFormItems = useAppSelector(selectFormDataItems);
+
   return dataFormItems.length > 0 ? (
-    dataFormItems.map((item) => <DataItem key={item.name} dataItem={item} />)
+    <div className={styles.list}>
+      {dataFormItems.map((item, index) => (
+        <DataItem
+          key={item.name}
+          dataItem={item}
+          className={index === dataFormItems.length - 1 ? styles.last_item : ''}
+        />
+      ))}
+    </div>
   ) : (
     <div>Нет данных для отображения, сделай submit формы</div>
   );
--- src/components/data-list/data-item/DataItem.tsx
+++ src/components/data-list/data-item/DataItem.tsx
@@ -5,15 +5,16 @@ import styles from './DataItem.module.css';
 
 type DataItemProps = {
   dataItem: FormDataItem;
+  className: string;
 };
 
-function DataItem({ dataItem }: DataItemProps) {
+function DataItem({ dataItem, className }: DataItemProps) {
   const { avatar, ...data } = dataItem;
   const file = convertBase64toFile(avatar);
   const objectUrl = URL.createObjectURL(file);
 
   return (
-    <div className={styles.item}>
+    <div className={`${styles.item} ${className}`}>
       <img className={styles.avatar} src={objectUrl} alt="avatar" />
       {Object.entries(data).map(([key, value]) => (
         <DataField key={key} name={key} value={value} />
