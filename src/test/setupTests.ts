507669bc686e97979d95f7ba0c236586f6eca2cc
Shakhrii <sh.ramaldanova@gmail.com>
2025-08-25 02:11:48 +0300

    refactor: remove unuseful code

--- src/test/setupTests.ts
+++ src/test/setupTests.ts
@@ -1,18 +1,1 @@
 import '@testing-library/jest-dom';
-import { server } from '../server/server';
-import { pokemonApi } from '../store/slices/api/pokemonApi';
-import { setupStore } from '../store/store';
-import { afterAll, afterEach, beforeAll } from 'vitest';
-
-const store = setupStore({});
-
-beforeAll(() => {
-  server.listen();
-});
-
-afterEach(() => {
-  server.resetHandlers();
-  store.dispatch(pokemonApi.util.resetApiState());
-});
-
-afterAll(() => server.close());
